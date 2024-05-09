bd2b37b9931cd423e2d56c9a0f857a6ecda1401a
Yang Qin <qin.yang@emotion-tech.co.jp>
2024-05-09 19:15:00 +0900

    branch

--- docs/github_pages/setup_rapidoc_index.sh
+++ docs/github_pages/setup_rapidoc_index.sh
@@ -5,21 +5,46 @@ sed -i -e "s|%PAGE_TITLE%|$PAGE_TITLE|g" $INDEX_FILE
 sed -i -e "s|%PAGE_FAVICON%|$PAGE_FAVICON|g" $INDEX_FILE
 sed -i -e "s|%RAPIDOC_OPTIONS%|${RAPIDOC_OPTIONS}|g" $INDEX_FILE
 
-sed -i -e "s|</rapi-doc>|<header slot='header' style='margin:auto 0;font-size:small;'></rapi-doc>|" $INDEX_FILE
-for service in $(cd $INDEX_BASE/et-api-doc; find . -maxdepth 1 -type d -not -name '.' -not -name dist -not -name sample -print | sort)
+for branch in $(cd $INDEX_BASE; find . -maxdepth 1 -type d -not -name '.' -not -name dist -not -name sample -print | sort)
 do
-  service=$(basename "$service")
-  sed -i -e "s|</rapi-doc>| <a href='../$service/' style='margin-right:1em;color:white;'>$service</a></rapi-doc>|" $INDEX_FILE
-done
-sed -i -e "s|</rapi-doc>|</header></rapi-doc>|" $INDEX_FILE
+  BRANCH_INDEX_BASE=$INDEX_BASE/$branch
+  BRANCH_INDEX_FILE=$BRANCH_INDEX_BASE/index.html
+  cp $INDEX_FILE $BRANCH_INDEX_FILE
+  sed -i -e "s|</rapi-doc>|<header slot='header' style='margin:auto 0;font-size:small;'></rapi-doc>|" $BRANCH_INDEX_FILE
+  for service in $(cd $BRANCH_INDEX_BASE/et-api-doc; find . -maxdepth 1 -type d -not -name '.' -not -name dist -not -name sample -print | sort)
+  do
+    service=$(basename "$service")
+    sed -i -e "s|</rapi-doc>| <a href='../$service/' style='margin-right:1em;color:white;'>$service</a></rapi-doc>|" $BRANCH_INDEX_FILE
+  done
+  sed -i -e "s|</rapi-doc>|</header></rapi-doc>|" $BRANCH_INDEX_FILE
 
-for service in $(cd $INDEX_BASE/et-api-doc; find . -maxdepth 1 -type d -not -name '.' -not -name dist -not -name sample -print)
+  for service in $(cd $BRANCH_INDEX_BASE/et-api-doc; find . -maxdepth 1 -type d -not -name '.' -not -name dist -not -name sample -print)
+  do
+    service=$(basename "$service")
+    mkdir "$BRANCH_INDEX_BASE/$service"
+    cp  $BRANCH_INDEX_FILE "$BRANCH_INDEX_BASE/$service/index.html"
+    sed -i -e "s|%SPEC_URL%|../et-api-doc/$service/index.yaml|" "$BRANCH_INDEX_BASE/$service/index.html"
+    sed -i -e 's|"rapidoc-min.js"|"../../rapidoc-min.js"|' "$BRANCH_INDEX_BASE/$service/index.html"
+  done
+
+cat <<'EOS' >$BRANCH_INDEX_FILE
+<!DOCTYPE html>
+<html>
+<body>
+<ul>
+EOS
+
+for service in $(cd $BRANCH_INDEX_BASE/et-api-doc; find . -maxdepth 1 -type d -not -name '.' -not -name dist -not -name sample -print | sort)
 do
   service=$(basename "$service")
-  mkdir "$INDEX_BASE/$service"
-  cp  $INDEX_FILE "$INDEX_BASE/$service/index.html"
-  sed -i -e "s|%SPEC_URL%|../et-api-doc/$service/index.yaml|" "$INDEX_BASE/$service/index.html"
-  sed -i -e 's|"rapidoc-min.js"|"../rapidoc-min.js"|' "$INDEX_BASE/$service/index.html"
+  echo "<li><a href='./$service/'>$service</a></li>" >> $BRANCH_INDEX_FILE
+done
+
+cat <<'EOS' >> $BRANCH_INDEX_FILE
+</ul>
+</body>
+EOS
+
 done
 
 cat <<'EOS' >$INDEX_FILE
@@ -29,10 +54,10 @@ cat <<'EOS' >$INDEX_FILE
 <ul>
 EOS
 
-for service in $(cd $INDEX_BASE/et-api-doc; find . -maxdepth 1 -type d -not -name '.' -not -name dist -not -name sample -print | sort)
+for branch in $(cd $INDEX_BASE; find . -maxdepth 1 -type d -not -name '.' -not -name dist -not -name sample -print | sort)
 do
-  service=$(basename "$service")
-  echo "<li><a href='./$service/'>$service</a></li>" >> $INDEX_FILE
+  branch=$(basename "$branch")
+  echo "<li><a href='./$branch/'>$branch</a></li>" >> $INDEX_FILE
 done
 
 cat <<'EOS' >> $INDEX_FILE
